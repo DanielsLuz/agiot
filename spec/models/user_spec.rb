5888cf3efb36741889418da325677d2efe3fd0fd
DanielsLuz <danielluz1995@gmail.com>
2016-07-30 22:18:03 -0300

    Added name validation for user name.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes){
+    { name: "FooUser" }
+  }
+
+  let(:invalid_attributes){
+    { name: "" }
+  }
+
+  it 'is valid' do
+    user = User.new(valid_attributes)
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user = User.new(name: nil)
+    expect(user).to be_invalid
+  end
 end
